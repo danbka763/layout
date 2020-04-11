4e10c374084010bddfc00d352d2d8a1b088c7d0c
Danil <48606033+danbka763@users.noreply.github.com>
2020-04-11 22:16:14 +0700

    Final push
    finish work

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ var ot = 0;
 
 function loadNewCard(ot) {
   
+  // POST запрос к серверу и получение данных
   let request = JSON.parse($.ajax({
     type: "POST",
     url: "https://tochka-market.space/api/getzayavki.php",
@@ -13,12 +14,24 @@ function loadNewCard(ot) {
     success: function(data) { return data }
   }).responseText);
 
+  // Если вернулся пустой массив - то удаляем элемент, на котором весит
+  // событие OnScreen, тем самым - останавливая последующее выполнение скрипта
+  // Удаляем скрипты со странички
+  if (request.length === 0) {
+    console.log('Stop OnScreen!');
+    document.getElementById('OS').remove();
+    document.getElementById('gettingCards').remove();
+    document.getElementById('scriptAJAX').remove();
+    return
+  }
+
+  // section дополняем элементами, которые вытащили запросом
   for (let i = 0; i < request.length; i++) {
     aside.innerHTML += `
-      <aside id="${request[i][0]}">
+      <aside id="card_${request[i][0]}">
         <div class="img_block"><img src="${request[i][2]}" alt=""></div>
         <div class="text-ad">
-          <h3>${request[i][1]}</h3>
+          <h3 class="name">${request[i][1]}</h3>
           <h3 class="price">${request[i][4]}  тг.</h3>
           <div class="time-and-number">
             <div class="time">${request[i][3]}</div>
